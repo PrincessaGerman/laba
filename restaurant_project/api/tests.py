5b0bd5aaa843e170683c0b95ce9b027c742cddbd
PrincessaGerman <dimagordimagor@mail.ru>
2025-02-28 23:06:23 +0300

    Cоставление тест-кейсов для проверки функциональности

--- restaurant_project/api/tests.py
+++ restaurant_project/api/tests.py
@@ -1,3 +1,8 @@
+# api/tests.py
 from django.test import TestCase
+from .models import MenuItem
 
-# Create your tests here.
+class MenuItemTest(TestCase):
+    def test_create_menu_item(self):
+        item = MenuItem.objects.create(name="IceCream", price=80, description="Delicious Ice Cream")
+        self.assertEqual(str(item), "IceCream")
